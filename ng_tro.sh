eafa3f57e2411c310cf587b7fb6f9e098e808920
MIC <weihua.zheng@hotmail.com>
2022-07-12 13:05:23 +0800

    Add files via upload
    update nginx installation and module installation

--- ng_tro.sh
+++ ng_tro.sh
@@ -3,6 +3,10 @@
 cd /home
 # 1.basic tools installation, git should already be installed before git onekey shell
 #yum -y install git
+yum -y install epel-release
+yum -y install yum-utils
+yum -y install nginx
+yum -y install nginx-mod-stream
 # 2. install docker-ce
 wget --no-check-certificate https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
 yum install -y yum-utils device-mapper-persistent-data lvm2
@@ -30,6 +34,8 @@ sudo certbot certonly --standalone -d www.dt-jj.com -d tro.dt-jj.com -m weihua.z
  yes|cp config.json /etc/trojan/
  # cp web files to relevant folder
  yes|cp -r /home/jjweb/* /usr/share/nginx/html/
+ # start nginx
+ systemctl start nginx.service && systemctl enable nginx.service
  # running trojan docker
 docker run -dit --privileged=true --name=trojan6 -p 10241:10241 -v /etc/letsencrypt/live/tro.dt-jj.com/fullchain.pem:/path/to/fullchain.pem -v /etc/letsencrypt/live/tro.dt-jj.com/privkey.pem:/path/to/privkey.pem -v /etc/trojan/config.json:/config/config.json fountain0/trojangfw:1.6
 
